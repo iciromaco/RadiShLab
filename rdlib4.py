67fa17c027d70921f9e3b11302f8a760ba34e35c
aquapathos <dan@ogu.ac.jp>
2020-02-11 09:24:07 +0900

    Add Sample Images and rdlib4

---
+++ rdlib4.py
@@ -0,0 +1,97 @@
+from glob import glob
+import os
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+# (1)画像のリストアップ
+# 指定フォルダ内の画像の収集
+from glob import glob
+import os
+from PIL import Image
+# 画像パスの収集
+def collectimagepaths(path, imgexts=['jpg','jpge','png']):
+    allfiles = glob(path+'/*')
+    imgfiles = []
+    for x in allfiles:
+        if os.path.splitext(x)[-1][1:] in imgexts:
+            imgfiles.append(x)
+    return imgfiles
+
+# 画像の収集
+def collectimages(path, imgexts=['jpg','jpge','png']):
+    imgfiles = collectimagepaths(path, imgexts)
+    imgs = [cv2.imread(afile,-1) for afile in imgfiles]
+    return imgs
+
+# サムネイルの作成
+def makethumbnail(path, imgexts=['jpg','jpge','png']):
+    imgfiles = collectimagepaths(path, imgexts)
+    i = 0
+    sam = Image.new('RGB', (500,100*((len(imgfiles)+4)//5)),(0,0,0))
+    row = col = 0
+    for rad in imgfiles:
+        img = Image.open(rad, 'r')
+        img.thumbnail((100, 100))
+        sam.paste(img,(col,row))
+        col += 100
+        if col == 500:
+            col = 0
+            row += 100
+    dirname = os.path.splitext(os.path.basename(path))[0]
+    sam.save('{}THUM.PNG'.format(dirname), 'PNG')
+    return sam
+
+# (2)画像の表示
+# プロット用関数
+def plotimg(img,layout="111"):
+    if len(img.shape) == 2:
+        pltgry(img,layout)
+    elif len(img.shape) ==3:
+        pltcol(img,layout)
+
+def pltgry(img,layout="111"):
+    plt.subplot(layout)
+    plt.axis('off')
+    plt.imshow(cv2.cvtColor(img,cv2.COLOR_GRAY2RGB))
+
+def pltcol(img,layout="111"):
+    plt.subplot(layout)
+    plt.axis('off')
+    plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+
+# (3) mkparaimage で２枚並べた画像を表示
+def mkparaimage(img1,img2):
+    h1,w1 = img1.shape[:2]
+    h2,w2 = img2.shape[:2]
+    if img1.ndim == 2:
+        img11 = np.zeros((h1,w1,3))
+        img11[:,:,0] = img11[:,:,1]=img11[:,:,2]=img1
+    else:
+        img11=img1
+    if img2.ndim == 2:
+        img22 = np.zeros((h2,w2,3))
+        img22[:,:,0]=img22[:,:,1]=img22[:,:,2]=img2
+    else:
+        img22=img2
+    paraimg = 255*np.ones((max(h1,h2),w1+w2+10,3),dtype=np.uint8)
+    
+    paraimg[0:h1,0:w1,:] = img11
+    paraimg[0:h2,w1+10:,:]=img22
+    
+    return paraimg
+
+# (4) マージンをつける
+def makemargin(img,mr=2):
+    h,w = img.shape[:2]
+    w2 = int(mr*w)
+    h2 = int(mr*h)
+    x1 = int((w2-w)/2)
+    y1 = int((h2-h)/2)
+    if len(img.shape)==2:
+        img2 = np.zeros((h2,w2),np.uint8)
+    else:
+        img2 = np.zeros((h2,w2,img.shape[2]),np.uint8)
+    img2[y1:y1+h,x1:x1+w] = img
+    return img2
