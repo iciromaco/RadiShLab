7bdec9c80fc8eb53e7b24f5e820b5ea9fd044283
aquapathos <dan@ogu.ac.jp>
2020-02-20 20:28:52 +0900

    202002202028

--- grabcut5.py
+++ grabcut5.py
@@ -70,37 +70,36 @@ Builder.load_string('''
 #:set IMGW 427
 <MyWidget>:
     size_hint: None,None
-    size: WINW, WINH
-    CanvasFloatLayout:
+    size: root.windowsize
+    FloatLayout:
         id: rdcanvas
         size_hint: None,None
-        # size: self.parent.size 
-        size: root.windowsize[0],root.srctexture.size[1]
+        size: WINW,IMGH
         pos: 0,BH        
         BoxLayout:
             orientation: 'horizontal'
             size_hint: None,None
-            size: root.windowsize[0],root.srctexture.size[1]
+            size: WINW,IMGH
             pos: 0,BH
             Image:
                 id: srcimg
                 size_hint: None,None
                 size: IMGW,IMGH
-                texture: root.srctexture
-                allow_stretch:True
+                # texture: root.srctexture
+                # allow_stretch:True
             Image:
                 id: outimg
                 size_hint: None,None
                 size: IMGW,IMGH
-                texture: root.outtexture
+                # texture: root.outtexture
     FloatLayout:
         size_hint: None,None
         size: WINW,BH
-        pos: 0,WINH-BH
+        pos: 0,root.top - BH
         BoxLayout:
             id: topmenu
             orientation: 'horizontal'
-            pos: 0,WINH-BH
+            pos: 0, root.top-BH
             Spinner:
                 size_hint_x: 0.2
                 id: sp0
@@ -224,6 +223,7 @@ def calculate_points(x1, y1, x2, y2, steps=5):
         o.extend([lastx, lasty])
     return o
 
+'''
 class CanvasFloatLayout(FloatLayout):
 
     def __init__(self,**kwargs):
@@ -250,21 +250,117 @@ class CanvasFloatLayout(FloatLayout):
         self.boxLayout.add_widget(self.outKvimg) 
         self.setsrcimg(DUMMYIMG)
 
-    def setsrcimg(self,srcimg):
-        self.srcCVimg = srcimg
-        h,w = srcimg.shape[:2]
-        self.boxLayout.texture = cv2kvtexture(srcimg)
-        self.gryCVimg = self.makegray()
-        self.outKvimg.texture = cv2kvtexture(self.gryCVimg)
+'''    
+# インタフェースパレット
+# opencv のカラー画像を kivy テキスチャに変換
+def cv2kvtexture(img):
+    if len(img.shape) == 2:
+        img2 = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+    else:
+        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # BGRからRGBへ
+    img2 = cv2.flip(img2,0)   # 上下反転
+    height = img2.shape[0]
+    width = img2.shape[1]
+    texture = Texture.create(size=(width,height))
+    texture.blit_buffer(img2.tostring())
+    return texture
+
+class MyWidget(BoxLayout):
+    windowsize = DUMMYIMG.shape[1]*2, DUMMYIMG.shape[0]+2*BUTTONH
+    # srctexture = cv2kvtexture(DUMMYIMG)
+    # outtexture = srctexture
+    res = GRC_RES
+    imgpath = DUMMYPATH
+    pictexture = {key:cv2kvtexture(picdic[key]) for key in picdic}
+    def __init__(self,**kwargs):
+        super(MyWidget,self).__init__(**kwargs)
+        self.setsrcimg(DUMMYIMG)
+        Clock.schedule_interval(self.update, 1)
 
     def makegray(self):
-        img = self.srcCVimg
+        img = self.srcimg
         if len(img.shape) == 3 :
             if img.shape[2] == 4: # Alpha チャネル付き
                 gray = cv2.cvtColor(img,cv2.COLOR_BGRA2GRAY)
             else:
                 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         return gray
+
+    def setsrcimg(self,srcimg):
+        self.srcimg = srcimg
+        h,w = srcimg.shape[:2]
+        self.srctexture = cv2kvtexture(srcimg)
+        self.ids['srcimg'].texture = self.srctexture
+        self.gryimg = self.makegray()
+        self.ids['rdcanvas'].size = (2*w,h)
+        self.ids['outimg'].texture = cv2kvtexture(self.gryimg)
+        self.ids['srcimg'].size = self.ids['outimg'].size = (w,h)
+        self.ids['topmenu'].pos = (0,h+IF_H)
+        
+
+    def makegray(self):
+        img = self.srcimg
+        if len(img.shape) == 3 :
+            if img.shape[2] == 4: # Alpha チャネル付き
+                gray = cv2.cvtColor(img,cv2.COLOR_BGRA2GRAY)
+            else:
+                gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        return gray
+
+    def update(self, dt):
+        h,w = self.srcimg.shape[:2]
+        Window.size = (2*w,h+2*IF_H)
+
+    # メニュー処理
+    def do_menu(self):
+        if self.ids['sp0'].text == 'File':
+            return
+        else:
+            self.mode = self.ids['sp0'].text
+            self.ids['sp0'].text = 'File'
+        if self.mode == 'Open':
+            self.show_load()
+        if self.mode == 'Save':
+            self.show_save()
+        if self.mode == 'Quit':
+            sys.exit()
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+        Window.size = self.keepsize
+ 
+    def show_load(self):
+        self.keepsize = Window.size
+        Window.size = (600,600)
+        content = Factory.LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9,0.9))
+        self._popup.open()
+ 
+    def load(self, filepath):
+        self.ids['path0'].text = filepath
+        srcimg = cv2.imread(filepath)
+        self.setsrcimg(srcimg)
+        self.dismiss_popup()
+
+    def show_save(self):
+        self.keepsize = Window.size
+        Window.size = (600,600)
+        content = Factory.SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+ 
+    def save(self, path, filename):
+        path = os.path.join(path, filename)
+        path1 = os.path.splitext(path)
+        if not path1[1].lower() in ['.png','.jpg']:
+            path = path1[0]+'.png'
+        self.ids['path0'].text = path
+        cv2.imwrite(path,self.outimg)
+        self.dismiss_popup()
+
+
 
     def on_touch_down(self, touch):
         if Widget.on_touch_down(self, touch):
@@ -331,105 +427,12 @@ class CanvasFloatLayout(FloatLayout):
         self.remove_widget(ud['label'])
     
     def update_touch_label(self, label, touch):
-        h,w = self.srcCVimg.shape[:2]
+        h,w = self.srcimg.shape[:2]
         y = (h + IF_H) - touch.y
         label.text = '(%d, %d)' % (touch.x, y)
         label.texture_update()
         label.pos = touch.pos
         label.size = label.texture_size[0] + 20, label.texture_size[1] + 20
-    
-# インタフェースパレット
-# opencv のカラー画像を kivy テキスチャに変換
-def cv2kvtexture(img):
-    if len(img.shape) == 2:
-        img2 = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
-    else:
-        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # BGRからRGBへ
-    img2 = cv2.flip(img2,0)   # 上下反転
-    height = img2.shape[0]
-    width = img2.shape[1]
-    texture = Texture.create(size=(width,height))
-    texture.blit_buffer(img2.tostring())
-    return texture
-
-class MyWidget(BoxLayout):
-    windowsize = DUMMYIMG.shape[1]*2, DUMMYIMG.shape[0]+2*BUTTONH
-    srctexture = cv2kvtexture(DUMMYIMG)
-    outtexture = srctexture
-    res = GRC_RES
-    imgpath = DUMMYPATH
-    m_size = BUTTONH
-    pictexture = {key:cv2kvtexture(picdic[key]) for key in picdic}
-    def __init__(self,**kwargs):
-        super(MyWidget,self).__init__(**kwargs)
-        self.rdcanvas = CanvasFloatLayout()
-        #self.rdcanvas.setsrcimg(DUMMYIMG)
-        #self.add_widget(self.rdcanvas)
-        self.ids['rdcanvas'] = self.rdcanvas
-        Clock.schedule_interval(self.update, 1)
-
-    def update(self, dt):
-        h,w = self.rdcanvas.srcCVimg.shape[:2]
-        Window.size = (2*w,h+2*IF_H)
-
-    # メニュー処理
-    def do_menu(self):
-        if self.ids['sp0'].text == 'File':
-            return
-        else:
-            self.mode = self.ids['sp0'].text
-            self.ids['sp0'].text = 'File'
-        if self.mode == 'Open':
-            self.show_load()
-        if self.mode == 'Save':
-            self.show_save()
-        if self.mode == 'Quit':
-            sys.exit()
-
-    def dismiss_popup(self):
-        self._popup.dismiss()
-        Window.size = self.keepsize
- 
-    def show_load(self):
-        self.keepsize = Window.size
-        Window.size = (600,600)
-        content = Factory.LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load file", content=content,
-                            size_hint=(0.9,0.9))
-        self._popup.open()
- 
-    def load(self, filepath):
-        self.ids['path0'].text = filepath
-        srcimg = cv2.imread(filepath)
-        self.rdcanvas.setsrcimg(srcimg)
-        self.dismiss_popup()
-
-    def show_save(self):
-        self.keepsize = Window.size
-        Window.size = (600,600)
-        content = Factory.SaveDialog(save=self.save, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Save file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
- 
-    def save(self, path, filename):
-        path = os.path.join(path, filename)
-        path1 = os.path.splitext(path)
-        if not path1[1].lower() in ['.png','.jpg']:
-            path = path1[0]+'.png'
-        self.ids['path0'].text = path
-        cv2.imwrite(path,self.outimg)
-        self.dismiss_popup()
-
-    def makegray(self):
-        img = self.srcimg
-        if len(img.shape) == 3 :
-            if img.shape[2] == 4: # Alpha チャネル付き
-                gray = cv2.cvtColor(img,cv2.COLOR_BGRA2GRAY)
-            else:
-                gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        return gray
-
 
 def mkpensizeSample(size=5):
     pensizeCanvas = np.zeros((32,32),np.uint8)
